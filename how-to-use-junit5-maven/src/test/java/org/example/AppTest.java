e33eb00911a9694158fa619d0d8211cb760f8295
deepcloudlabs <deepcloudlabs@gmail.com>
2020-04-28 10:21:59 +0300

    add example to study @Tag and tag filtering

--- how-to-use-junit5-maven/src/test/java/org/example/AppTest.java
+++ how-to-use-junit5-maven/src/test/java/org/example/AppTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -7,7 +8,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    @Tag("functional")
+    public void test1() {
+        System.err.println("test #1");
+    }
+
+    @Test
+    @Tag("performance")
+    public void test2() {
+        System.err.println("test #2");
+    }
+
+    @Test
+    @Tag("stress")
+    public void test3() {
+        System.err.println("test #3");
     }
 }
